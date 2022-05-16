6a619da92ec7de6e2d5d943e358e03faf64035ad
Alessandro Mosca <mosca.alessandro29@gmail.com>
2022-05-16 17:40:40 +0200

    app can now recognize actuation on

--- usbSerialExamples/src/main/java/com/hoho/android/usbserial/examples/myHandler.java
+++ usbSerialExamples/src/main/java/com/hoho/android/usbserial/examples/myHandler.java
@@ -17,27 +17,8 @@ public class myHandler extends Handler {
         Passer passer= (Passer) msg.obj;
         byte[] data= passer.data;
         int id = byteToInt(data[0]);
+        passer.handler.post(() -> passer.FCCurrent.setText(String.format("%d",id)));
         switch(id){
-            case 32://wheel :
-                int strategy=byteToInt(data[1]);
-                    passer.handler.post(() -> passer.strategy.setText(String.format("ST: %d",strategy)));
-                if(data[2]!=0)  //motor on
-                    passer.handler.post(() -> passer.motorOn.setBackgroundColor(Color.parseColor("#FF0000")));
-                else            //motor off
-                    passer.handler.post(() -> passer.motorOn.setBackgroundColor(Color.TRANSPARENT));
-                if(data[3]!=0)  //purge on
-                    passer.handler.post(() -> passer.purge.setBackgroundColor(Color.parseColor("#00FF00")));
-                else            //purge off
-                    passer.handler.post(() -> passer.purge.setBackgroundColor(Color.TRANSPARENT));
-                if(data[4]!=0)  //powerMode on
-                    passer.handler.post(() -> passer.SCVoltage.setBackgroundColor(Color.parseColor("#FF00FF")));
-                else            //powerMode off
-                    passer.handler.post(() -> passer.SCVoltage.setBackgroundColor(Color.TRANSPARENT));
-                if(data[5]!=0)  //short on
-                    passer.handler.post(() -> passer._short.setBackgroundColor(Color.parseColor("#FFFF00")));
-                else            //short off
-                    passer.handler.post(() -> passer._short.setBackgroundColor(Color.TRANSPARENT));
-                break;
             case 16://service board: emergences
                 boolean emActive=false;
                 for(int i=1;i<5;i++) {
@@ -65,9 +46,34 @@ public class myHandler extends Handler {
                 float SCVoltage=byteToFloat(data[4],data[3],data[2],data[1]);
                 passer.handler.post(() -> passer.SCVoltage.setText(String.format("%.2f V",SCVoltage)));
                 break;
+            case 32://wheel :
+                int strategy=byteToInt(data[1]);
+                passer.handler.post(() -> passer.strategy.setText(String.format("ST: %d",strategy)));
+                if(data[2]!=0)  //motor on
+                    passer.handler.post(() -> passer.motorOn.setBackgroundColor(Color.parseColor("#FF0000")));
+                else            //motor off
+                    passer.handler.post(() -> passer.motorOn.setBackgroundColor(Color.TRANSPARENT));
+                if(data[3]!=0)  //purge on
+                    passer.handler.post(() -> passer.purge.setBackgroundColor(Color.parseColor("#00FF00")));
+                else            //purge off
+                    passer.handler.post(() -> passer.purge.setBackgroundColor(Color.TRANSPARENT));
+                if(data[4]!=0)  //powerMode on
+                    passer.handler.post(() -> passer.SCVoltage.setBackgroundColor(Color.parseColor("#FF00FF")));
+                else            //powerMode off
+                    passer.handler.post(() -> passer.SCVoltage.setBackgroundColor(Color.TRANSPARENT));
+                if(data[5]!=0)  //short on
+                    passer.handler.post(() -> passer._short.setBackgroundColor(Color.parseColor("#FFFF00")));
+                else            //short off
+                    passer.handler.post(() -> passer._short.setBackgroundColor(Color.TRANSPARENT));
+                break;
             case 48://actuation board: FCCurrent
                 float FCCurrent=byteToFloat(data[4],data[3],data[2],data[1]);
                 passer.handler.post(() -> passer.FCCurrent.setText(String.format("%.2f A",FCCurrent)));
+                break;
+            case 63://actuation board heartbeat
+                passer.handler.post(() -> passer.actuationOn.setBackgroundColor(Color.parseColor("#0000FF")));
+                if(data[1]<1)
+                    passer.handler.post(() -> passer.actuationOn.setBackgroundColor(Color.TRANSPARENT));
                 break;
             default:
                 break;
